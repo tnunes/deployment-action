3efeae24ffb4cd21d21fd347dd3fc03fe6b3202f
Tiago Nunes <me@tiagonunes.pt>
2020-07-19 18:56:43 +0100

    feat: add transient_environment and production_environment params

--- dist/index.js
+++ dist/index.js
@@ -3208,31 +3208,34 @@ function run() {
         try {
             const context = github.context;
             const logUrl = `https://github.com/${context.repo.owner}/${context.repo.repo}/commit/${context.sha}/checks`;
-            const token = core.getInput("token", { required: true });
-            const ref = core.getInput("ref", { required: false }) || context.ref;
-            const url = core.getInput("target_url", { required: false }) || logUrl;
-            const environment = core.getInput("environment", { required: false }) || "production";
-            const description = core.getInput("description", { required: false });
-            const initialStatus = core.getInput("initial_status", {
-                required: false
-            }) || "pending";
-            const autoMergeStringInput = core.getInput("auto_merge", {
-                required: false
+            const token = core.getInput('token', { required: true });
+            const ref = core.getInput('ref', { required: false }) || context.ref;
+            const url = core.getInput('target_url', { required: false }) || logUrl;
+            const environment = core.getInput('environment', { required: false }) || 'production';
+            const productionEnvironment = core.getInput('production_environment', { required: false }) || 'true';
+            const transientEnvironment = core.getInput('transient_environment', { required: false }) || 'false';
+            const description = core.getInput('description', { required: false });
+            const initialStatus = core.getInput('initial_status', {
+                required: false,
+            }) || 'pending';
+            const autoMergeStringInput = core.getInput('auto_merge', {
+                required: false,
             });
-            const auto_merge = autoMergeStringInput === "true";
-            const client = new github.GitHub(token, { previews: ["flash", "ant-man"] });
+            const auto_merge = autoMergeStringInput === 'true';
+            const client = new github.GitHub(token, { previews: ['flash', 'ant-man'] });
             const deployment = yield client.repos.createDeployment({
                 owner: context.repo.owner,
                 repo: context.repo.repo,
                 ref: ref,
                 required_contexts: [],
                 environment,
-                transient_environment: true,
+                production_environment: productionEnvironment === 'true',
+                transient_environment: transientEnvironment === 'true',
                 auto_merge,
-                description
+                description,
             });
             yield client.repos.createDeploymentStatus(Object.assign({}, context.repo, { deployment_id: deployment.data.id, state: initialStatus, log_url: logUrl, environment_url: url }));
-            core.setOutput("deployment_id", deployment.data.id.toString());
+            core.setOutput('deployment_id', deployment.data.id.toString());
         }
         catch (error) {
             core.error(error);
--- src/main.ts
+++ src/main.ts
@@ -1,62 +1,67 @@
-import * as core from "@actions/core";
-import * as github from "@actions/github";
+import * as core from '@actions/core';
+import * as github from '@actions/github';
 
 type DeploymentState =
-  | "error"
-  | "failure"
-  | "inactive"
-  | "in_progress"
-  | "queued"
-  | "pending"
-  | "success";
+	| 'error'
+	| 'failure'
+	| 'inactive'
+	| 'in_progress'
+	| 'queued'
+	| 'pending'
+	| 'success';
 
 async function run() {
-  try {
-    const context = github.context;
-    const logUrl = `https://github.com/${context.repo.owner}/${context.repo.repo}/commit/${context.sha}/checks`;
+	try {
+		const context = github.context;
+		const logUrl = `https://github.com/${context.repo.owner}/${context.repo.repo}/commit/${context.sha}/checks`;
 
-    const token = core.getInput("token", { required: true });
-    const ref = core.getInput("ref", { required: false }) || context.ref;
-    const url = core.getInput("target_url", { required: false }) || logUrl;
-    const environment =
-      core.getInput("environment", { required: false }) || "production";
-    const description = core.getInput("description", { required: false });
-    const initialStatus =
-      (core.getInput("initial_status", {
-        required: false
-      }) as DeploymentState) || "pending";
-    const autoMergeStringInput = core.getInput("auto_merge", {
-      required: false
-    });
+		const token = core.getInput('token', { required: true });
+		const ref = core.getInput('ref', { required: false }) || context.ref;
+		const url = core.getInput('target_url', { required: false }) || logUrl;
+		const environment =
+			core.getInput('environment', { required: false }) || 'production';
+		const productionEnvironment =
+			core.getInput('production_environment', { required: false }) || 'true';
+		const transientEnvironment =
+			core.getInput('transient_environment', { required: false }) || 'false';
+		const description = core.getInput('description', { required: false });
+		const initialStatus =
+			(core.getInput('initial_status', {
+				required: false,
+			}) as DeploymentState) || 'pending';
+		const autoMergeStringInput = core.getInput('auto_merge', {
+			required: false,
+		});
 
-    const auto_merge: boolean = autoMergeStringInput === "true";
+		const auto_merge: boolean = autoMergeStringInput === 'true';
 
-    const client = new github.GitHub(token, { previews: ["flash", "ant-man"] });
+		const client = new github.GitHub(token, { previews: ['flash', 'ant-man'] });
 
-    const deployment = await client.repos.createDeployment({
-      owner: context.repo.owner,
-      repo: context.repo.repo,
-      ref: ref,
-      required_contexts: [],
-      environment,
-      transient_environment: true,
-      auto_merge,
-      description
-    });
+		const deployment = await client.repos.createDeployment({
+			owner: context.repo.owner,
+			repo: context.repo.repo,
+			ref: ref,
+			required_contexts: [],
+			environment,
+			production_environment: productionEnvironment === 'true',
+			transient_environment: transientEnvironment === 'true',
+			auto_merge,
+			description,
+		});
 
-    await client.repos.createDeploymentStatus({
-      ...context.repo,
-      deployment_id: deployment.data.id,
-      state: initialStatus,
-      log_url: logUrl,
-      environment_url: url
-    });
+		await client.repos.createDeploymentStatus({
+			...context.repo,
+			deployment_id: deployment.data.id,
+			state: initialStatus,
+			log_url: logUrl,
+			environment_url: url,
+		});
 
-    core.setOutput("deployment_id", deployment.data.id.toString());
-  } catch (error) {
-    core.error(error);
-    core.setFailed(error.message);
-  }
+		core.setOutput('deployment_id', deployment.data.id.toString());
+	} catch (error) {
+		core.error(error);
+		core.setFailed(error.message);
+	}
 }
 
 run();
